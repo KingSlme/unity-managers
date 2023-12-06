10e80e23d99901e29e6b3c3416719f2d49859d94
KingSlme <djgreene100@gmail.com>
2023-12-05 23:03:59 -0500

    Add initial scripts

---
+++ Managers/AssetManager.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/*
+*Remember to attach this script to a prefab named AssetManager within the Resources folder*
+
+Public Methods:
+- GetMusicAudioClip()
+- GetSFXAudioClip()
+- GetUITexture()
+- GetPrefab()
+*/
+public class AssetManager : Singleton<AssetManager>
+{    
+    public enum MusicEnum
+    {
+
+    }
+    public enum SFXEnum
+    {
+
+    }
+    public enum UITextureEnum
+    {
+
+    }
+    public enum PrefabEnum
+    {
+
+    }
+
+    private Dictionary<MusicEnum, AudioClip> _musicDictionary = new Dictionary<MusicEnum, AudioClip>();
+    private Dictionary<SFXEnum, AudioClip> _sfxDictionary = new Dictionary<SFXEnum, AudioClip>();
+    private Dictionary<UITextureEnum, Texture> _uiTextureDictionary = new Dictionary<UITextureEnum, Texture>();
+    private Dictionary<PrefabEnum, GameObject> _prefabDictionary = new Dictionary<PrefabEnum, GameObject>();
+
+    private static AssetManager _instance;
+
+    public new static AssetManager Instance
+    {
+        get
+        {   
+            if (_instance == null)
+            {   
+                _instance = FindObjectOfType<AssetManager>();
+                if (_instance == null)
+                {
+                    _instance = (Instantiate(Resources.Load("AssetManager")) as GameObject).GetComponent<AssetManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+    
+    // Assets:
+    [Header("Music")] 
+    [SerializeField] private MusicAudioClip[] _musicArray;
+    [Header("SFX")]
+    [SerializeField] private SFXAudioClip[] _sfxArray;
+    [Header("Textures")] 
+    [SerializeField] private UITexture[] _uiTextureArray;
+    [Header("Prefabs")] 
+    [SerializeField] private Prefab[] _prefabArray;
+
+    [System.Serializable] public class MusicAudioClip
+    {
+        public MusicEnum MusicAudioClipMember;
+        public AudioClip MusicAudioClipAsset;
+    }
+
+    [System.Serializable] public class SFXAudioClip
+    {
+        public SFXEnum SFXAudioClipMember;
+        public AudioClip SFXAudioClipAsset;
+    }
+    [System.Serializable] public class UITexture
+    {
+        public UITextureEnum UITextureMember;
+        public Texture UITextureAsset;
+    }
+    [System.Serializable] public class Prefab
+    {
+        public PrefabEnum PrefabMember;
+        public GameObject PrefabAsset;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        InitializeDictionaries(_musicArray, _musicDictionary);
+        InitializeDictionaries(_sfxArray, _sfxDictionary);
+        InitializeDictionaries(_uiTextureArray, _uiTextureDictionary);
+        InitializeDictionaries(_prefabArray, _prefabDictionary);
+    }
+
+    private void InitializeDictionaries<T, U, V>(T[] assetArray, Dictionary<U, V> dictionary) where V : class
+    {
+        foreach (T asset in assetArray)
+        {
+            var assetMember = (U)asset.GetType().GetField($"{typeof(T).Name}Member").GetValue(asset); 
+            var assetField = (V)asset.GetType().GetField($"{typeof(T).Name}Asset").GetValue(asset);
+            if (!assetField.Equals(null)) {
+                dictionary.Add(assetMember, assetField); 
+            }
+        }
+    }
+
+    private V GetAsset< U, V>(Dictionary<U, V> dictionary, U assetKey) where V : class
+    {
+        if (dictionary.TryGetValue(assetKey, out V assetValue))
+        {
+            return assetValue;
+        }
+        Debug.LogError($"Missing {assetKey} Asset!"); 
+        return null;
+    }
+
+    public AudioClip GetMusicAudioClip(MusicEnum music) => GetAsset(_musicDictionary, music);  
+    public AudioClip GetSFXAudioClip(SFXEnum sfx) => GetAsset(_sfxDictionary, sfx);
+    public Texture GetUITexture(UITextureEnum texture) => GetAsset(_uiTextureDictionary, texture);
+    public GameObject GetPrefab(PrefabEnum prefab) => GetAsset(_prefabDictionary, prefab);
+}
---
+++ Managers/AudioManager.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+Public Methods:
+- PlayMusic()
+- StopMusic()
+- ResumeMusic()
+- TransitionMusic()
+- PlaySFX()
+- PlaySFXAtPoint()
+- CreateMusicSource()
+- GetRandomSFX()
+- ChangeMasterVolume()
+- ToggleSFX()
+- ToggleMusic()
+*/
+public class AudioManager : Singleton<AudioManager>
+{       
+    private AudioSource _musicSourceA;
+    private AudioSource _musicSourceB;
+    private AudioSource _sfxSource;
+    private Coroutine _fadeInCoroutine;
+    private Coroutine _fadeOutCoroutine;
+    private float _fadeDuration = 2.0f; // Duration for fading in and fading out 
+    
+    protected override void Awake() 
+    {   
+        base.Awake();
+        if (_musicSourceA == null)
+        {
+            _musicSourceA = gameObject.AddComponent<AudioSource>();
+            _musicSourceA.loop = true;
+        }
+        if (_musicSourceB == null)
+        {
+            _musicSourceB = gameObject.AddComponent<AudioSource>();
+            _musicSourceB.loop = true;
+        }
+        if (_sfxSource == null)
+        {
+            _sfxSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlayMusic(AssetManager.MusicEnum musicEnum, float volume = 1.0f) 
+    {   
+        // _musicSourceA is treated as the primary Audio Source
+        _musicSourceB.clip = null;
+        StopMusic();
+        _musicSourceA.volume = volume;
+        _musicSourceA.clip = AssetManager.Instance.GetMusicAudioClip(musicEnum);
+        _musicSourceA.Play();
+    }
+
+    public void StopMusic()
+    {   
+        _musicSourceA.Stop();
+        _musicSourceB.Stop();
+    }
+
+    public void ResumeMusic()
+    {
+        if (_musicSourceA.clip != null)
+        {
+            _musicSourceA.UnPause();
+        }
+        if (_musicSourceB.clip != null)
+        {
+            _musicSourceB.UnPause();
+        }
+    }
+
+    public void TransitionMusic(AssetManager.MusicEnum musicEnum, float volume = 1.0f)
+    {   
+        if (_fadeInCoroutine != null)
+        {
+            StopCoroutine(_fadeInCoroutine);
+        }
+        if (_fadeOutCoroutine != null)
+        {   
+            StopCoroutine(_fadeOutCoroutine);
+        }
+
+        if (_musicSourceA.isPlaying)
+        {   
+            _fadeOutCoroutine = StartCoroutine(FadeOut(_musicSourceA, volume));
+            _fadeInCoroutine = StartCoroutine(FadeIn(_musicSourceB, musicEnum, volume));
+        }
+        else if (_musicSourceB.isPlaying)
+        {
+            _fadeOutCoroutine = StartCoroutine(FadeOut(_musicSourceB, volume));
+            _fadeInCoroutine = StartCoroutine(FadeIn(_musicSourceA, musicEnum, volume));
+        }
+        else
+        {
+            _fadeInCoroutine = StartCoroutine(FadeIn(_musicSourceA, musicEnum, volume));
+        }
+    }
+
+    private IEnumerator FadeIn(AudioSource musicSource, AssetManager.MusicEnum musicEnum, float volume)
+    {   
+        musicSource.volume = 0;
+        musicSource.clip = AssetManager.Instance.GetMusicAudioClip(musicEnum);
+        musicSource.Play();
+        float elapsedTime = 0.0f;
+        while (elapsedTime < _fadeDuration)
+        {
+            musicSource.volume = Mathf.Lerp(0, volume, elapsedTime / _fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        musicSource.volume = volume;
+    }
+
+    private IEnumerator FadeOut(AudioSource musicSource, float volume)
+    {   
+        musicSource.volume = volume;
+        float elapsedTime = 0.0f;
+        while (elapsedTime < _fadeDuration)
+        {
+            musicSource.volume = Mathf.Lerp(volume, 0, elapsedTime / _fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        musicSource.volume = 0;
+        musicSource.Stop();
+        musicSource.clip = null;
+    }
+
+    public void PlaySFX(AssetManager.SFXEnum sfxEnum, float volume = 1.0f) => _sfxSource.PlayOneShot(AssetManager.Instance.GetSFXAudioClip(sfxEnum), volume);
+
+    public void PlaySFXAtPoint(AssetManager.SFXEnum sfxEnum, Vector3 position, float volume = 1.0f, float minDistance = 1.0f, float maxDistance = 500.0f)
+    {
+        GameObject tempObject = CreateSpatialAudioObject(position, volume, minDistance, maxDistance);
+        AudioSource audioSource = tempObject.GetComponent<AudioSource>();
+        audioSource.clip = AssetManager.Instance.GetSFXAudioClip(sfxEnum);
+        audioSource.Play();
+        Destroy(tempObject, audioSource.clip.length);
+    }
+
+    public GameObject CreateMusicSource(AssetManager.MusicEnum musicEnum, Vector3 position, float volume = 1.0f, float minDistance = 1.0f, float maxDistance = 500.0f)
+    {
+        GameObject audioSourceObject = CreateSpatialAudioObject(position, volume, minDistance, maxDistance);
+        AudioSource audioSource = audioSourceObject.GetComponent<AudioSource>();
+        audioSource.loop = true;
+        audioSource.clip = AssetManager.Instance.GetMusicAudioClip(musicEnum);
+        audioSource.Play();
+        return audioSourceObject;
+    }
+
+    private GameObject CreateSpatialAudioObject(Vector3 position, float volume = 1.0f, float minDistance = 1.0f, float maxDistance = 500.0f)
+    {
+        GameObject audioSourceObject = new GameObject("3D Spatialization Audio");
+        audioSourceObject.transform.position = position;
+        AudioSource audioSource = audioSourceObject.AddComponent<AudioSource>();
+        audioSource.volume = volume;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.minDistance = minDistance;
+        audioSource.maxDistance = maxDistance;
+        return audioSourceObject;
+    }
+
+    public AssetManager.SFXEnum GetRandomSFX(params AssetManager.SFXEnum[] sfxEnums) => sfxEnums[Random.Range(0, sfxEnums.Length)];
+
+    public void ChangeMasterVolume(float value) => AudioListener.volume = value;
+    public void ToggleSFX() => _sfxSource.mute = !_sfxSource.mute;
+    public void ToggleMusic()
+    {
+        _musicSourceA.mute = !_musicSourceA.mute;
+        _musicSourceB.mute = !_musicSourceB.mute;
+    } 
+}
